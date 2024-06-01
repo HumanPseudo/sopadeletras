93ae8639eabc4b1f11bd099bdc16e4dd4d52f6f1
HumanPseudo <cadalagu@hotmail.com>
2024-05-31 21:23:17 -0500

    ok

--- src/app/page.js
+++
@@ -1,8 +0,0 @@
-'use client';
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Sopa de letras</h1>
-    </main>
-  );
-}
