a81660167a77e6d43f36a06ac90dc00f3c6e79a7
HumanPseudo <cadalagu@hotmail.com>
2024-05-31 20:31:18 -0500

    migrate to next

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+'use client';
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Sopa de letras</h1>
+    </main>
+  );
+}
